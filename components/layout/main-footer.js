d71884c6a3137f9802a471a2171ba0ae79740b75
Stu Last <stuart@last-family.co.uk>
2021-12-02 19:11:39 +0000

    Fix footer url visibility

--- components/layout/main-footer.js
+++ components/layout/main-footer.js
@@ -1,12 +1,23 @@
-import styles from './main-footer.module.css'
+import styles from './main-footer.module.css';
 
 const MainFooter = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div>
-          <p>This website is purely a demo website and does not present any real events, dates, or addresses.</p>
-          <p>The public source code for this demo website can be found at <a href="https://github.com/StuLast/NextJS-demo-events-page">https://github.com/StuLast/NextJS-demo-events-page</a></p>
+          <p>
+            This website is purely a demo website and does not present any real
+            events, dates, or addresses.
+          </p>
+          <p>
+            The public source code for this demo website can be found at{' '}
+            <a
+              href='https://github.com/StuLast/NextJS-demo-events-page'
+              target='_blank'
+            >
+              https://github.com/StuLast/NextJS-demo-events-page
+            </a>
+          </p>
           <p>This website was built by Stu Last of Spyced Concepts</p>
         </div>
       </div>
